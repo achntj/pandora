edaa0647e847b7d1cc9bf8c7261d245abc2ab29a
achintyajha <achintyakjha@gmail.com>
2022-06-05 21:33:46 +0530

    remove published data

--- pages/api/publish/[id].ts
+++ pages/api/publish/[id].ts
@@ -1,15 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
 
 // PUT /api/publish/:id
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const postId = req.query.id;
 
-
-    const post = await prisma.post.update({
-      where: { id: Number(postId) },
-      data: { published: true },
-    });
-    res.json(post);
-  
+  const post = await prisma.post.update({
+    where: { id: Number(postId) },
+    data: {},
+  });
+  res.json(post);
 }
