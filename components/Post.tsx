4b420762199c275662dbe1d8654898927fb1db7f
achintyajha <achintyakjha@gmail.com>
2022-07-20 20:09:01 +0530

    init

--- components/Post.tsx
+++ components/Post.tsx
@@ -6,11 +6,13 @@ export type PostProps = {
   title: string;
   content: string;
   date: Date;
+  complete: boolean;
 };
 
 const Post: React.FC<{ post: PostProps }> = ({ post }) => {
   return (
     <div>
+      <p>{post.complete}</p>
       <p className="m-0 text-2xl text-pink-500 font-medium">{post.title}</p>
       <div className="entry">
         <ReactMarkdown children={post.content} />
