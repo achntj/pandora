0593601d855b3dec1b01deb1e2459dde3dafc845
achintyajha <achintyakjha@gmail.com>
2022-07-20 23:04:08 +0530

    read/unread feature added

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { MailIcon, MailOpenIcon } from "@heroicons/react/outline";
 
 export type PostProps = {
   id: number;
@@ -10,10 +11,47 @@ export type PostProps = {
 };
 
 const Post: React.FC<{ post: PostProps }> = ({ post }) => {
+  const [complete, setComplete] = useState(post.complete);
+  const updateComplete = async (id: number, body: boolean) => {
+    const pass = prompt("Enter Password");
+    if (pass !== null) {
+      if (pass === process.env.NEXT_PUBLIC_PASS) {
+        setComplete(!complete);
+        try {
+          await fetch(
+            `${
+              process.env.VERCEL_URL || "http://localhost:3000"
+            }/api/complete/${id}`,
+            {
+              method: "PUT",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(body),
+            }
+          );
+        } catch (error) {
+          alert(error);
+        }
+      } else {
+        alert("Wrong Password hahahah");
+      }
+    }
+  };
   return (
     <div>
-      <p>{post.complete}</p>
-      <p className="m-0 text-2xl text-pink-500 font-medium">{post.title}</p>
+      <p
+        className={`m-0 text-2xl ${
+          complete ? "text-emerald-600" : "text-pink-500"
+        } font-medium flex items-center justify-between`}
+      >
+        {complete && "[COMPLETE]"} {post.title}
+        <span onClick={() => updateComplete(post.id, complete)}>
+          {complete ? (
+            <MailOpenIcon className="h-6 w-6" />
+          ) : (
+            <MailIcon className="h-6 w-6" />
+          )}
+        </span>
+      </p>
       <div className="entry">
         <ReactMarkdown children={post.content} />
       </div>
--- pages/api/complete/[id].ts
+++ pages/api/complete/[id].ts
@@ -1,16 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
 
-// PUT /api/publish/:id
+// PUT /api/complete/:id
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const postId = req.query.id;
+  const isComplete = req.body;
 
   const post = await prisma.post.update({
     where: { id: Number(postId) },
-    data: {},
+    data: {
+      complete: !isComplete,
+    },
   });
   res.json(post);
 }
